68c3c071ee87773bebce8fc9354de064ada6f8d6
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 20:22:46 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -1,12 +1,18 @@
-marker2 = "EY"
+#Example 2
+
+marker = "EY"
 replacement = "Eyjafjallajokull"
 line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe for 6 days."
 
 x = line.find("E")
 y = line.find("Y", x)
 
-#print x        #by printing x and y we get index at which first occurance of "E" and "Y" is present in the string passed in "Line" variable
+# z variable will store resultant value
+z = line[:x] + replacement + line[y + 1:]          # I'm directly passing value of x and y to reduce the LINE OF CODE.
+#z1 = line[:28] + replacement + line[30 + 1:]       # By passing value of index we get the same output, un-comment to check the same.
+
+#print x        #by printing x and y we get index of "E" and "Y" within the string passed in "Line" variable
 #print y + 1    # You can un-comment the print statements to see the results
 
-print line[:x] + replacement + line[y + 1:]
-#print line[:28] + replacement + line[30 + 1:]  # By passing value of index we get the same output, un-comment to check the same
+print z
+#print z1       #Un-comment "z1" variable before un-commenting this !!
