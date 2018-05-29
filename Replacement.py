ab240cda1d07b1a63754bb9ae2ed56b4d34f2961
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 19:43:34 +0530

    Update Replacement.py
    There are 2 different ways to Solve this. I had embedded both ways within ONE File by Commenting few lines.

--- Replacement.py
+++ Replacement.py
@@ -5,7 +5,8 @@ line = "I will now go to sleep and be AFK until lunch time tomorrow."
 x = line.find("A")
 y = line.find("K", x)
 
-print x
-print y + 1
+#print x        #by printing x and y we get index of "A" and "K" within the string passed in "Line" variable
+#print y + 1    # You can un-comment the print statements to see the results
 
-print line[:x] + replacement + line[y + 1:]
+print line[:x] + replacement + line[y + 1:]     # I'm directly passing value of x and y to reduce the LINE OF CODE.
+#print line[:30] + replacement + line[32 + 1:]  # By passing value of index we get the same output, un-comment to check the same.
