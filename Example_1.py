2346ece993fcb0c97f35c41799fb851787b3ed8f
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 20:14:39 +0530

    Update Example_1.py

--- Example_1.py
+++ Example_1.py
@@ -6,9 +6,12 @@ line = "I will now go to sleep and be AFK until lunch time tomorrow."
 
 x = line.find("A")
 y = line.find("K", x)
+# z variable will store resultant value
+z = line[:x] + replacement + line[y + 1:]          # I'm directly passing value of x and y to reduce the LINE OF CODE.
+#z1 = line[:30] + replacement + line[32 + 1:]       # By passing value of index we get the same output, un-comment to check the same.
 
 #print x        #by printing x and y we get index of "A" and "K" within the string passed in "Line" variable
 #print y + 1    # You can un-comment the print statements to see the results
 
-print line[:x] + replacement + line[y + 1:]     # I'm directly passing value of x and y to reduce the LINE OF CODE.
-#print line[:30] + replacement + line[32 + 1:]  # By passing value of index we get the same output, un-comment to check the same.
+print z
+#print z1        #Un-comment "z1" variable before un-commenting this !!
