06df115c23c02279169155f48d656dcc7058f043
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 20:00:01 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -1,11 +1,11 @@
-﻿marker2 = "EY"
+marker2 = "EY"
 replacement = "Eyjafjallajokull"
 line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe for 6 days."
 
 x = line.find("E")
 y = line.find("Y", x)
 
-#print x        #by printing x and y we get index of "A" and "K" within the string passed in "Line" variable
+#print x        #by printing x and y we get index at which first occurance of "E" and "Y" is present in the string passed in "Line" variable
 #print y + 1    # You can un-comment the print statements to see the results
 
 print line[:x] + replacement + line[y + 1:]
