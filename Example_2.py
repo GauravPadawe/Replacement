480c01fbba78caf962cd24dd07a1f67a3b8dffd9
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 19:56:48 +0530

    Add files via upload

---
+++ Example_2.py
@@ -0,0 +1,12 @@
+﻿marker2 = "EY"
+replacement = "Eyjafjallajokull"
+line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe for 6 days."
+
+x = line.find("E")
+y = line.find("Y", x)
+
+#print x        #by printing x and y we get index of "A" and "K" within the string passed in "Line" variable
+#print y + 1    # You can un-comment the print statements to see the results
+
+print line[:x] + replacement + line[y + 1:]
+#print line[:28] + replacement + line[30 + 1:]  # By passing value of index we get the same output, un-comment to check the same
