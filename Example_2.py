f47871ab2aaebf22b97a76d111ad48fd4381a0f3
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-29 21:21:55 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -9,7 +9,7 @@ y = line.find("Y", x)
 
 # z variable will store resultant value
 z = line[:x] + replacement + line[y + 1:]          # I'm directly passing value of x and y to reduce the LINE OF CODE.
-#z1 = line[:28] + replacement + line[30 + 1:]       # By passing value of index we get the same output, un-comment to check the same.
+#z1 = line[:28] + replacement + line[30 + 1:]      # By passing value of index we get the same output, un-comment to check the same.
 
 #print x        #by printing x and y we get index of "E" and "Y" within the string passed in "Line" variable
 #print y + 1    # You can un-comment the print statements to see the results
