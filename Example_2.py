750787f7197768e95fa9b93a957d9b4676bec9d0
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-30 21:22:54 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -7,14 +7,14 @@ line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe fo
 x = line.find("E")
 y = line.find("Y", x)
 
-# z variable will store resultant value
-z = line[:x] + replacement + line[y + 1:]          # I'm directly passing value of x and y to reduce the LINE OF CODE.
+# "replaced" variable will store resultant value
+replaced = line[:x] + replacement + line[y + 1:]          # I'm directly passing value of x and y to reduce the LINE OF CODE.
 #z1 = line[:28] + replacement + line[30 + 1:]      # By passing value of index we get the same output, un-comment to check the same.
 
 #print x        #by printing x and y we get index of "E" and "Y" within the string passed in "Line" variable
 #print y + 1    # You can un-comment the print statements to see the results
 
-print z
+print replaced
 #print z1       #Un-comment "z1" variable before un-commenting this !!
 
 # CODED BY - GAURAV PADAWE
